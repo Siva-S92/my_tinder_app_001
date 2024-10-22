67e8c122606c387087ab880b088caff068ecbb82
Siva-S92 <sivagraphics4fashion@gmail.com>
2024-10-22 18:22:16 +0530

    prepare to render deployment

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,2 +1,2 @@
-export const serverURL = "https://my-tinder-app-001-backend.vercel.app";
-// export const serverURL = "http://localhost:5000";
+// export const serverURL = "https://my-tinder-app-001-backend.vercel.app";
+export const serverURL = "http://localhost:5000";
--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { serverURL } from '../constant';
+// import { serverURL } from '../constant';
 
 
 
@@ -7,6 +7,7 @@ import { serverURL } from '../constant';
 
 export const axiosInstance = axios.create({
     // TO DO UPDATE THE BASEURL HERE, SO THAT IT WORKS IN THE DEPLOYMENT AS WELL 
-    baseURL: `${serverURL}/api`,
+    // baseURL: `${serverURL}/api`,
+    baseURL: "/api",
     withCredentials: true,
 })
